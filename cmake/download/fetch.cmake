09eae130c18ec5d5e11dfd1bab385092e27d1a96
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-10-05 23:08:55 -0400

    Add external project iniparser

---
+++ cmake/download/fetch.cmake
@@ -0,0 +1,32 @@
+
+include(ExternalProject)
+
+ExternalProject_Add(
+	iniparser
+	GIT_REPOSITORY https://github.com/ndevilla/iniparser
+	GIT_TAG				 v4.1
+	GIT_SHALLOW		 1
+	GIT_PROGRESS	 1
+	BUILD_IN_SOURCE 1
+	CONFIGURE_COMMAND ""
+	BUILD_COMMAND make
+	INSTALL_COMMAND ""
+	COMMAND  ${CMAKE_COMMAND} -E copy_if_different "<SOURCE_DIR>/src/iniparser.h"
+		"${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/iniparser/inc"
+	COMMAND  ${CMAKE_COMMAND} -E copy_if_different "<SOURCE_DIR>/src/dictionary.h"
+		"${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/iniparser/inc"
+	COMMAND  ${CMAKE_COMMAND} -E copy_if_different "<SOURCE_DIR>/libiniparser.a"
+		"${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/iniparser/lib"
+	COMMAND  ${CMAKE_COMMAND} -E copy_if_different "<SOURCE_DIR>/libiniparser.so.1"
+		"${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/iniparser/lib"
+	)
+
+# FetchContent_MakeAvailable(iniparser)
+
+# FetchContent_GetProperties(iniparser)
+	# if(NOT iniparser_POPULATED)
+	# FetchContent_Populate(iniparser)
+
+	# # Build the C++ version of iniparser as well
+	# add_subdirectory(${reproc_INCLUDE_DIR})
+# endif()
